cf1a38f31c4937eb0bdaac38eae3ae46b50f766e
Evgeny Polyakov <evg_pol@yahoo.com>
2022-10-13 21:55:12 +0300

    first commit

---
+++ src/components/Game.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Board from './Board'
+import './Game.css'
+
+export default function Game() {
+    return (
+        <div className='wrapper'>
+            <Board />
+        </div>
+    )
+}
---
+++ src/components/Square.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Square.css'
+
+export default function Square() {
+    return (
+        <button className='square'>x</button>
+    )
+}
